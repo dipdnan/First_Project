7ac207c91f6a08b30b250c77e64dfe8313454626
dipdnan <nanawaredeepak895@gmail.com>
2023-06-13 10:28:46 +0530

    My first scala programs

--- src/main/scala/week1/first.scala
+++ src/main/scala/week1/first.scala
@@ -4,6 +4,18 @@ object first
 {
   def main(args: Array[String]) {
     println("Hello Scala");
-    println("Hello World !!")
+    println("Hello World !!");
+    println("Hello Scala");
+    println("Hello World !!");
+
+    println("Hello Scala");
+    println("Hello World !!");
+    println("Hello Scala");
+    println("Hello World !!");
+    println("Hello Scala");
+    println("Hello World !!");
+    println("Hello Scala");
+    println("Hello World !!");
+
   }
 }
